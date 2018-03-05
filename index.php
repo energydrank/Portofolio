86d684383a39d2b083c9277fc39bc51d3b6981a1
77604 <77604@rocteraa-student.nl>
2018-03-05 12:17:47 +0100

    Added Initial Files to update and change Later, Nothing added to index.php yet

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Portofolio">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Portofolio Jesse Siersema</title>
+</head>
