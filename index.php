f6ecf697f3ed744017bf31838d46032afa58161e
77604 <77604@rocteraa-student.nl>
2018-03-16 14:26:52 +0100

    Added some images

--- index.php
+++ index.php
@@ -1,8 +1,115 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<meta name="description" content="Portofolio">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Portofolio Jesse Siersema</title>
-</head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+
+    <title>Portofolio Jesse Siersema</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-md">
+  <a class="navbar-brand" href="#">Portofolio Jesse Siersema</a>
+  <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+</nav>
+
+
+<div class="jumbotron" style="background-image: url('img/Jumbotron.jpg'); background-size: 1903px 290px; ">
+  <div class="container">
+    <h1 class="display-4" style=" Color: White;">Welcome</h1>
+		<hr class="my-4">
+    <p class="lead" style="Color: White;"> My Name is Jesse Siersema</p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mb-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">
+            <h4>About me</h4>
+          </div>
+          <div class="card-body">
+            <p class="lead">About me</p>
+            <p>I am an 18 year old Student from the ROC Ter Aa where i study Application Developer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <img class="img-thumbnail" src="img/code.jpg" />
+      <p> Note: This is a Placeholder Image</p>
+    </div>
+    <div class="col-md-4">
+      <img class="img-thumbnail" src="img/code.jpg" />
+      <p> Note: This is a Placeholder Image</p>
+    </div>
+
+    <div class="col-md-8 mb-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">
+            <h4>About me</h4>
+          </div>
+          <div class="card-body">
+            <p class="lead">About me</p>
+            <p>I am an 18 year old Student from the ROC Ter Aa where i study Application Developer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+<!-- Row Closing Tag Reminder-->
+  </div>
+  <!-- Container Closing Tag -->
+</div>
+
+      <div class="card" style="background-color: purple;">
+        <div class="card-body">
+          <blockquote class="blockquote mb-4">
+            <h1 style="color: white;">" There are three responses to a piece of Design: yes,no and WOW! Wow is the one to aim for.""</h1>
+            <footer class="blockquote-footer" style="color: White">Milton Glaser </footer>
+          </blockquote>
+        </div>
+      </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mt-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">
+            <h4>About me</h4>
+          </div>
+          <div class="card-body">
+            <p class="lead">About me</p>
+            <p>I am an 18 year old Student from the ROC Ter Aa where i study Application Developer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 mt-4">
+      <img class="img-thumbnail" src="img/code.jpg" />
+      <p> Note: This is a Placeholder Image</p>
+    </div>
+  </div>
+
+  </div>
+
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"</script>
+  </body>
+</html>
